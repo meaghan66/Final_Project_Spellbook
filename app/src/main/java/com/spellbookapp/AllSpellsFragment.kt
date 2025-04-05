d4b024ac9295ff885afde5383c98234b9114b55d
Lauren Bushman <lkf57@nau.edu>
2025-04-05 11:59:12 -0700

    Initial commit

---
+++ app/src/main/java/com/spellbookapp/AllSpellsFragment.kt
@@ -0,0 +1,6 @@
+package com.spellbookapp
+
+import androidx.fragment.app.Fragment
+
+class AllSpellsFragment : Fragment(R.layout.fragment_all_spells){
+}
---
+++ app/src/main/java/com/spellbookapp/PreparedSpellsFragment.kt
@@ -0,0 +1,6 @@
+package com.spellbookapp
+
+import androidx.fragment.app.Fragment
+
+class PreparedSpellsFragment : Fragment(R.layout.fragment_prepared_spells) {
+}
